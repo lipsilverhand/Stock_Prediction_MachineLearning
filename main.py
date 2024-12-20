041176384073266c6006ea684c742714135d1213
lipsilverhand <williamhuynh99@gmail.com>
2024-12-20 10:18:01 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+import yfinance as yf
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import MinMaxScaler
+import openai 
+
